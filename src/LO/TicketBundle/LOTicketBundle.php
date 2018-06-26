56fb15f4330ac2686603b368ee3dbfc3543bdbe4
jeremie <jkenobi@free.fr>
2018-06-26 19:59:14 +0200

    refactor name
    rename of bundle, controller and routing.yml

---
+++ src/LO/TicketBundle/LOTicketBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace LO\TicketBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LOTicketBundle extends Bundle
+{
+}
--- src/TicketBundle/TicketBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace TicketBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class TicketBundle extends Bundle
-{
-}
